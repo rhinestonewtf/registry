3c2f63e4aca9892c3e28991840867ee1d9622fff
zeroknots <zeroknots@protonmail.com>
2023-06-26 08:07:55 +0700

    added RSQuery interface and documentation

--- src/base/RSQuery.sol
+++ src/base/RSQuery.sol
@@ -4,14 +4,36 @@ pragma solidity ^0.8.19;
 import {
     AccessDenied, NotFound, NO_EXPIRATION_TIME, InvalidLength, uncheckedInc
 } from "../Common.sol";
+import "../interface/IRQuery.sol";
 import "./RSAttestation.sol";
+
+import "forge-std/console2.sol";
 
 /// @title RSRegistry
 /// @author zeroknots
 /// @notice The global attestation registry.
-abstract contract RSQuery {
+abstract contract RSQuery is IRQuery {
     error RevokedAttestation(bytes32 attestationId);
 
+    /**
+     * @inheritdoc IRSQuery
+     */
+    function verifyWithRevert(
+        address module,
+        address authority
+    )
+        public
+        view
+        returns (bool verified)
+    {
+        bytes32 uid = _getAttestation(module, authority);
+        _verifyAttestation(uid);
+        verified = true;
+    }
+
+    /**
+     * @inheritdoc IRSQuery
+     */
     function verifyWithRevert(
         address module,
         address[] memory authorities,
@@ -25,19 +47,25 @@ abstract contract RSQuery {
         if (length < threshold || threshold == 0) threshold = length;
 
         for (uint256 i; i < length; uncheckedInc(i)) {
-            address authority = authorities[i];
-            bytes32 uid = _getAttestation(module, authority);
             if (threshold == 0) return true;
-            if (uid != EMPTY_UID) {
-                _verifyAttestation(uid);
-                --threshold;
-            }
+            verifyWithRevert(module, authorities[i]);
+            --threshold;
         }
         return false;
     }
 
+    /**
+     * @inheritdoc IRSQuery
+     */
+    function verifyWithRevert(bytes32 attestationId) public view returns (bool verified) {
+        _verifyAttestation(attestationId);
+        verified = true;
+    }
+
+    /**
+     * @inheritdoc IRSQuery
+     */
     function verifyWithRevert(
-        address module,
         bytes32[] memory attestationIds,
         uint256 threshold
     )
@@ -50,19 +78,15 @@ abstract contract RSQuery {
 
         for (uint256 i; i < length; uncheckedInc(i)) {
             if (threshold == 0) return true;
-            _verifyAttestation(attestationIds[i]);
+            verifyWithRevert(attestationIds[i]);
             --threshold;
         }
         return false;
     }
 
-    function _verifyAttestation(bytes32 attestationId) internal view {
-        Attestation storage attestation = _getAttestation(attestationId);
-        bytes32 refUID = attestation.refUID;
-        if (attestation.revocationTime != 0) revert RevokedAttestation(attestationId);
-        if (refUID != EMPTY_UID) _verifyAttestation(refUID); // @TODO security issue?
-    }
-
+    /**
+     * @inheritdoc IRSQuery
+     */
     function findAttestation(
         address module,
         address authority
@@ -73,6 +97,13 @@ abstract contract RSQuery {
     {
         bytes32 attestionId = _getAttestation(module, authority);
         attestation = _getAttestation(attestionId);
+    }
+
+    function _verifyAttestation(bytes32 attestationId) internal view {
+        Attestation storage attestation = _getAttestation(attestationId);
+        bytes32 refUID = attestation.refUID;
+        if (attestation.revocationTime != 0) revert RevokedAttestation(attestationId);
+        if (refUID != EMPTY_UID) _verifyAttestation(refUID); // @TODO security issue?
     }
 
     function _getAttestation(
---
+++ src/interface/IRSQuery.sol
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import { Attestation } from "../Common.sol";
+/**
+ * RSQuery interface allows for the verification of attestations
+ * with potential for reversion in case of invalid attestation.
+ *
+ * @author zeroknots
+ */
+
+interface IRSQuery {
+    /**
+     * Verify an attestation associated with a given module and authority. Revert if the attestation is invalid.
+     *
+     * @param module The address of the module to verify
+     * @param authority The address of the authority issuing the attestation
+     * @return verified True if the attestation is valid
+     */
+    function verifyWithRevert(
+        address module,
+        address authority
+    )
+        external
+        view
+        returns (bool verified);
+
+    /**
+     * Verify a set of attestations associated with a given module and a list of authorities. Revert if any attestation is invalid.
+     *
+     * @param module The address of the module to verify
+     * @param authorities The list of authorities issuing the attestations
+     * @param threshold The minimum number of valid attestations required
+     * @return verified True if the number of valid attestations is at least the threshold
+     */
+    function verifyWithRevert(
+        address module,
+        address[] memory authorities,
+        uint256 threshold
+    )
+        external
+        view
+        returns (bool verified);
+
+    /**
+     * Verify an attestation using its id. Revert if the attestation is invalid.
+     *
+     * @param attestationId The id of the attestation to verify
+     * @return verified True if the attestation is valid
+     */
+    function verifyWithRevert(bytes32 attestationId) external view returns (bool verified);
+
+    /**
+     * Verify a set of attestations using their ids. Revert if any attestation is invalid.
+     *
+     * @param attestationIds The ids of the attestations to verify
+     * @param threshold The minimum number of valid attestations required
+     * @return verified True if the number of valid attestations is at least the threshold
+     */
+    function verifyWithRevert(
+        bytes32[] memory attestationIds,
+        uint256 threshold
+    )
+        external
+        view
+        returns (bool verified);
+
+    /**
+     * Find an attestation associated with a given module and authority.
+     *
+     * @param module The address of the module
+     * @param authority The address of the authority issuing the attestation
+     * @return attestation The attestation associated with the module and authority
+     */
+    function findAttestation(
+        address module,
+        address authority
+    )
+        external
+        view
+        returns (Attestation memory attestation);
+}
