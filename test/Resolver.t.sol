a72ac4affbf37c3eae61a2215784357c86524936
zeroknots <zeroknots@protonmail.com>
2024-02-07 16:14:32 +0700

    chore: adding resolver tests

--- test/Resolver.t.sol
+++ test/Resolver.t.sol
@@ -1,17 +1,27 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-contract ResolverTest {
+import "./Base.t.sol";
+
+contract ResolverTest is BaseTest {
     modifier whenRegisteringResolver() {
         _;
     }
 
-    function test_WhenNewResolver() external whenRegisteringResolver {
+    function test_WhenNewResolver() external whenRegisteringResolver prankWithAccount(opsEntity1) {
         // It should work.
+
+        MockResolver newResolver = new MockResolver(false);
+        registry.registerResolver(IExternalResolver(address(newResolver)));
     }
 
     function test_WhenResolverAlreadyRegistered() external whenRegisteringResolver {
         // It should revert.
+
+        MockResolver newResolver = new MockResolver(false);
+        registry.registerResolver(IExternalResolver(address(newResolver)));
+        vm.expectRevert();
+        registry.registerResolver(IExternalResolver(address(newResolver)));
     }
 
     modifier whenUpdatingResolver() {
@@ -20,9 +30,24 @@ contract ResolverTest {
 
     function test_WhenUsingUnauthorizedAccount() external whenUpdatingResolver {
         // It should revert.
+        MockResolver newResolver = new MockResolver(false);
+        vm.prank(opsEntity1.addr);
+        ResolverUID resolverUID = registry.registerResolver(IExternalResolver(address(newResolver)));
+
+        vm.expectRevert();
+        registry.setResolver(resolverUID, IExternalResolver(address(newResolver)));
     }
 
     function test_WhenUsingAuthorizedAccount() external whenUpdatingResolver {
-        // It should update.
+        MockResolver newResolver = new MockResolver(false);
+        vm.prank(opsEntity1.addr);
+        ResolverUID resolverUID = registry.registerResolver(IExternalResolver(address(newResolver)));
+
+        MockResolver newResolver2 = new MockResolver(false);
+        vm.prank(opsEntity1.addr);
+        registry.setResolver(resolverUID, IExternalResolver(address(newResolver2)));
+
+        // ResolverRecord memory record = registry.resolvers(resolverUID);
+        // assertEq(address(record.resolver), address(newResolver2));
     }
 }
