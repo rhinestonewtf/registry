4892745ea13293b4af0a9a7319d6e6ed9d8b8397
zeroknots <zeroknots@protonmail.com>
2024-04-04 12:20:49 +0700

    chore: move setter of trusted attesters to IERC7484 interface

--- src/IRegistry.sol
+++ src/IRegistry.sol
@@ -29,6 +29,17 @@ interface IERC7484 {
 
     function checkForAccount(address smartAccount, address module, ModuleType moduleType) external view;
 
+    /**
+     * Allows Smart Accounts - the end users of the registry - to appoint
+     * one or many attesters as trusted.
+     * @dev this function reverts, if address(0), or duplicates are provided in attesters[]
+     *
+     * @param threshold The minimum number of attestations required for a module
+     *                  to be considered secure.
+     * @param attesters The addresses of the attesters to be trusted.
+     */
+    function trustAttesters(uint8 threshold, address[] calldata attesters) external;
+
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
     /*              Check with external attester(s)               */
     /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
@@ -69,17 +80,6 @@ interface IRegistry is IERC7484 {
     error AttestationNotFound();
 
     error InsufficientAttestations();
-
-    /**
-     * Allows Smart Accounts - the end users of the registry - to appoint
-     * one or many attesters as trusted.
-     * @dev this function reverts, if address(0), or duplicates are provided in attesters[]
-     *
-     * @param threshold The minimum number of attestations required for a module
-     *                  to be considered secure.
-     * @param attesters The addresses of the attesters to be trusted.
-     */
-    function trustAttesters(uint8 threshold, address[] calldata attesters) external;
 
     /**
      * Get trusted attester for a specific Smart Account
