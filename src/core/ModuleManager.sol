4af3776fb9dc7d9e65dad3314aa5da3273a69b7e
zeroknots <zeroknots@protonmail.com>
2024-04-18 10:49:14 +0700

    fix: passing msg.value
    wip

--- src/core/ModuleManager.sol
+++ src/core/ModuleManager.sol
@@ -26,7 +26,7 @@ contract FactoryTrampolin {
         bool success;
         /* solhint-disable no-inline-assembly */
         assembly ("memory-safe") {
-            success := call(gas(), factory, 0, add(callOnFactory, 0x20), mload(callOnFactory), 0, 32)
+            success := call(gas(), factory, callvalue(), add(callOnFactory, 0x20), mload(callOnFactory), 0, 32)
             moduleAddress := mload(0)
         }
         if (!success) {
@@ -139,7 +139,7 @@ abstract contract ModuleManager is IRegistry, ResolverManager {
 
         // Call the factory via the trampolin contract. This will make sure that there is msg.sender separation
         // Making "raw" calls to user supplied addresses could create security issues.
-        moduleAddress = FACTORY_TRAMPOLIN.deployViaFactory({ factory: factory, callOnFactory: callOnFactory });
+        moduleAddress = FACTORY_TRAMPOLIN.deployViaFactory{ value: msg.value }({ factory: factory, callOnFactory: callOnFactory });
 
         ModuleRecord memory record = _storeModuleRecord({
             moduleAddress: moduleAddress,
