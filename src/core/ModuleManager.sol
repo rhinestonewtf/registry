f58041748ca1028fc1f9c0df34b008acb4ad6d0e
zeroknots <102132718+zeroknots@users.noreply.github.com>
2024-04-19 22:29:08 +0700

    Update src/core/ModuleManager.sol
    Co-authored-by: Konrad <kopp.konrad@gmail.com>

--- src/core/ModuleManager.sol
+++ src/core/ModuleManager.sol
@@ -137,7 +137,7 @@ abstract contract ModuleManager is IRegistry, ResolverManager {
         // prevent someone from calling a registry function pretending its a factory
         if (factory == address(this)) revert FactoryCallFailed(factory);
 
-        // Call the factory via the trampolin contract. This will make sure that there is msg.sender separation
+        // Call the factory via the trampoline contract. This will make sure that there is msg.sender separation
         // Making "raw" calls to user supplied addresses could create security issues.
         moduleAddress = FACTORY_TRAMPOLINE.deployViaFactory{ value: msg.value }({ factory: factory, callOnFactory: callOnFactory });
 
